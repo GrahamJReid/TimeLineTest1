8da8b871a1c443ed2f5762f5805fe961faaad4aa
GrahamJReid <grahamreidtechnology@gmail.com>
2023-08-19 10:28:31 -0500

    remvd upload form extract timeline.js

--- pages/timeline.js
+++ pages/timeline.js
@@ -1,9 +1,48 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import { useEffect, useState } from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
 function Timeline() {
+  const [sortedEventArray, setSortedEventArray] = useState([]);
+
+  const eventArray = [
+    {
+      id: 1,
+      title: 'title 1',
+      date: '2023-05-19',
+      imageUrl: 'https://t3.ftcdn.net/jpg/03/41/22/66/360_F_341226661_rH9SmwMoNxShy66rJ9Zy1m7zQCJfaXvH.jpg',
+      description: 'this is an apple that was the first apple ever',
+      color: 'red',
+    },
+    {
+      id: 2,
+      title: 'title 2',
+      date: '2000-06-10',
+      imageUrl: 'https://spaces-cdn.clipsafari.com/548zvxnhj2o3ayw92sf7q5d0t66q',
+      description: 'this is an banana that was the first banana ever',
+      color: 'orange',
+    },
+    {
+      id: 3,
+      title: 'title 3',
+      date: '2002-09-09',
+      imageUrl: 'https://clipartix.com/wp-content/uploads/2016/08/Grapes-clipart-3.png',
+      description: 'grapes are great for real',
+      color: 'purple',
+    },
+  ];
+  useEffect(() => {
+    // Create a copy of the eventArray and sort it by date
+    const sortedEvents = [...eventArray].sort((a, b) => new Date(a.date) - new Date(b.date));
+    setSortedEventArray(sortedEvents);
+  }, []);
+
   return (
     <div>
+      <input type="color" />
       <VerticalTimeline>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
@@ -97,6 +136,26 @@ function Timeline() {
             this timeline app gonna blow your mind
           </p>
         </VerticalTimelineElement>
+
+        <div>
+          {sortedEventArray.map((event) => (
+            <VerticalTimelineElement
+              contentStyle={{ background: `${event.color}`, color: '#fff' }}
+              contentArrowStyle={{ borderRight: `7px solid  ${event.color}` }}
+              date={event.date}
+              iconStyle={{ background: `${event.color}`, color: '#fff' }}
+            >
+              <h3 className="vertical-timeline-element-title">{event.title}</h3>
+              <img src={event.imageUrl} />
+              <h5>description: {event.description}</h5>
+
+              <p>
+                {event.date}
+              </p>
+            </VerticalTimelineElement>
+
+          ))}
+        </div>
       </VerticalTimeline>
 
     </div>
